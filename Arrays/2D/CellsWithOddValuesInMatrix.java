203df350140e46e695f9f302f09afb411a0a3686
Sharad Nanda <5526742+GeekNandy@users.noreply.github.com>
2021-02-04 23:38:59 +0530

    Create CellsWithOddValuesInMatrix.java

---
+++ Arrays/2D/CellsWithOddValuesInMatrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int oddCells(int n, int m, int[][] indices) {
+        boolean[] oddRows = new boolean[n], oddCols = new boolean[m];
+        int cntRow = 0, cntCol = 0;
+        for (int[] idx : indices) {
+            oddRows[idx[0]] ^= true;
+            oddCols[idx[1]] ^= true;
+            cntRow += oddRows[idx[0]] ? 1 : -1;
+            cntCol += oddCols[idx[1]] ? 1 : -1;
+        }
+        return (m - cntCol) * cntRow + (n - cntRow) * cntCol;
+    }
+}
